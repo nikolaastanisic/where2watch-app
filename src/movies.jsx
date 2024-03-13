1da192551cc0a4d19c8cb5837355547fdd6d3e76
Your Name <nstanisic21@gmail.com>
2024-03-13 13:03:11 +0100

    Where to watch modul added

--- src/movies.jsx
+++ src/movies.jsx
@@ -1,113 +1,103 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Header from "@components/header.jsx";
-import "@scss/search-bar.scss"
-import "@scss/movies.scss"
+import "@scss/search-bar.scss";
+import "@scss/movies.scss";
+import WhereToWatchModal from "./watch-modal";
 
 const Movies = () => {
- const [defaultMovies, setDefaultMovies] = useState([]);
- const [searchMovies, setSearchMovies] = useState([]);
- const [searchTerm, setSearchTerm] = useState('');
- const [genres, setGenres] = useState([]);
- const [watchProviders, setWatchProviders] = useState({}); 
- const [providersList, setProvidersList] = useState({}); 
+  const [defaultMovies, setDefaultMovies] = useState([]);
+  const [searchMovies, setSearchMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [genres, setGenres] = useState([]);
 
- useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=3ebdcea237cba8f1759952fa1674302f&language=en-US`)
-      .then(res => res.json())
-      .then(json => setGenres(json.genres));
- }, []);
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=3ebdcea237cba8f1759952fa1674302f&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((json) => setGenres(json.genres));
+  }, []);
 
- useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=3ebdcea237cba8f1759952fa1674302f&sort_by=popularity.desc`)
-      .then(res => res.json())
-      .then(json => setDefaultMovies(json.results));
- }, []);
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=3ebdcea237cba8f1759952fa1674302f&sort_by=popularity.desc`
+    )
+      .then((res) => res.json())
+      .then((json) => setDefaultMovies(json.results));
+  }, []);
 
- useEffect(() => {
+  useEffect(() => {
     if (searchTerm) {
-      fetch(`https://api.themoviedb.org/3/search/movie?api_key=3ebdcea237cba8f1759952fa1674302f&query=${searchTerm}`)
-        .then(res => res.json())
-        .then(json => setSearchMovies(json.results));
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=3ebdcea237cba8f1759952fa1674302f&query=${searchTerm}`
+      )
+        .then((res) => res.json())
+        .then((json) => setSearchMovies(json.results));
     }
- }, [searchTerm]);
+  }, [searchTerm]);
 
- useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/watch/providers/movie?api_key=3ebdcea237cba8f1759952fa1674302f`)
-      .then(res => res.json())
-      .then(json => {
-        const providers = json.results;
-        const providersList = providers.reduce((acc, curr) => ({ ...acc, [curr.id]: curr.name }), {});
-        setProvidersList(providersList);
-      });
- }, []);
-
- const fetchWatchProviders = (movieId) => {
-  fetch(`https://api.themoviedb.org/3/movie/${movieId}/watch/providers?api_key=3ebdcea237cba8f1759952fa1674302f`)
-     .then(res => res.json())
-     .then(json => {
-       const providers = { [movieId]: json.results.US };
-       setWatchProviders(prevProviders => ({ ...prevProviders, ...providers }));
-     });
- };
-
- const handleSearchChange = (e) => {
+  const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
- };
+  };
 
- const getGenreNames = (movie) => {
-    return movie.genre_ids.map(id => genres.find(genre => genre.id === id).name).join(', ');
- };
+  const getGenreNames = (movie) => {
+    return movie.genre_ids
+      .map((id) => genres.find((genre) => genre.id === id).name)
+      .join(", ");
+  };
 
- const getProviderNames = (movieId) => {
-    const providers = watchProviders[movieId] || {};
-    console.log('Providers for movieId', movieId, ':', providers); // Debugging line
-    return Object.values(providers).map(providerId => providersList[providerId]).join(', ');
- };
-
- return (
+  return (
     <>
       <Header key={self.crypto.randomUUID()} />
-      <div className='all-movies'>
-        <div className='movies-header'>
+      <div className="all-movies">
+        <div className="movies-header">
           <h1>Movies</h1>
           <p>Find your desired movie here!</p>
           <div className="search-container">
-          <label>
-            <input
-              type="text"
-              placeholder='Search movie'
-              value={searchTerm}
-              onChange={handleSearchChange}
-            />
-          </label>
+            <label>
+              <input
+                type="text"
+                placeholder="Search movie"
+                value={searchTerm}
+                onChange={handleSearchChange}
+              />
+            </label>
           </div>
         </div>
-        <hr></hr>
-        <div className='movies-list'>
-          {searchTerm ? searchMovies.map((movie) => (
-            <div className='card' key={movie.id}>
-              <img className='posters' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-              <h2>{movie.title}</h2>
-              <p>Release Date: {movie.release_date.replace(/-/g, '.')}</p>
-              <p>Rating : {movie.vote_average}</p>
-              <p>Genres: {getGenreNames(movie)}</p>
-              <button onClick={() => fetchWatchProviders(movie.id)}>Watch it here</button>  
-              <p>Available on: {getProviderNames(movie.id)}</p>           
-            </div>
-          )) : defaultMovies.map((movie) => (
-            <div className='card' key={movie.id}>
-              <img className='posters' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-              <h2>{movie.title}</h2>
-              <p>Release Date: {movie.release_date.replace(/-/g, '.')}</p>
-              <p>Rating : {movie.vote_average}</p>
-              <p>Genres: {getGenreNames(movie)}</p>
-              <button onClick={() => fetchWatchProviders(movie.id)}>Watch it here</button>
-              <p>Available on: {getProviderNames(movie.id)}</p>            </div>
-          ))}
+        <div className="movies-list">
+          {searchTerm
+            ? searchMovies.map((movie) => (
+                <div className="card" key={movie.id}>
+                  <img
+                    className="posters"
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                  <h2>{movie.title}</h2>
+                  <p>Release Date: {movie.release_date.replace(/-/g, ".")}</p>
+                  <p>Rating : {movie.vote_average}</p>
+                  <p>Genres: {getGenreNames(movie)}</p>
+                  <WhereToWatchModal movie={movie} />
+                </div>
+              ))
+            : defaultMovies.map((movie) => (
+                <div className="card" key={movie.id}>
+                  <img
+                    className="posters"
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                  <h2>{movie.title}</h2>
+                  <p>Release Date: {movie.release_date.replace(/-/g, ".")}</p>
+                  <p>Rating : {movie.vote_average}</p>
+                  <p>Genres: {getGenreNames(movie)}</p>
+                  <p><WhereToWatchModal movie={movie} /></p>
+                </div>
+              ))}
         </div>
       </div>
     </>
- );
+  );
 };
 
 export default Movies;
--- src/watch-modal.jsx
+++ src/watch-modal.jsx
@@ -1,18 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Slide from "@mui/material/Slide"; // Import Slide transition
+import { Avatar } from "@mui/material";
+import "@scss/watch-modal.scss";
+import { useMediaQuery } from '@mui/material';
 
-const Modal = ({ isOpen, onClose, children }) => {
- if (!isOpen) {
-    return null;
- }
-
- return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={e => e.stopPropagation()}>
-        {children}
-        <button onClick={onClose}>Close</button>
-      </div>
-    </div>
- );
+const style = {
+ position: "absolute",
+ top: "50%",
+ left: "40%",
+ transform: "translate(-50%, -50%)",
+ width: 500,
+ bgcolor: "#3a5a4045",
+ border: "2px solid #3a5a40",
+ boxShadow: 24,
+ p: 4,
 };
 
-export default Modal;
+export default function WhereToWatchModal({ movie }) {
+ const [watchProviders, setWatchProviders] = useState([]);
+ const isMobile = useMediaQuery('(max-width:600px)');
+ const fetchWatchProviders = (movieId) => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/watch/providers?api_key=3ebdcea237cba8f1759952fa1674302f`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        const providers =
+          json?.results?.US?.rent?.map(
+            (provider) =>
+              (provider = {
+                logo: provider.logo_path,
+                name: provider.provider_name,
+              })
+          ) ?? [];
+        setWatchProviders(providers);
+      });
+ };
+
+ const [open, setOpen] = React.useState(false);
+ const handleOpen = () => {
+  document.body.classList.add('no-scroll');
+  fetchWatchProviders(movie.id);
+  setOpen(true);
+ };
+ 
+ const handleClose = () => {
+  document.body.classList.remove('no-scroll');
+  setOpen(false);
+ };
+
+ return (
+  <div>
+    <Button onClick={handleOpen}>Where to watch</Button>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Slide direction="up" in={open} mountOnEnter unmountOnExit>
+        <Box sx={style}>
+          {watchProviders.length > 0 && (
+            <div className={`logos ${isMobile ? 'mobile' : ''}`}>
+              {watchProviders.map((provider, i) => (
+               <span key={i}>
+                  <img
+                    className="provider-logo"
+                    src={"https://image.tmdb.org/t/p/w500/" + provider.logo}
+                  />
+               </span>
+              ))}
+            </div>
+          )}
+          {watchProviders.length === 0 && <span className="not">Not streaming yet.</span>}
+        </Box>
+      </Slide>
+    </Modal>
+  </div>
+);
+
+}
