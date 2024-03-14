e03181767222b9df371931c8d7b8d91f325a1f04
Your Name <nstanisic21@gmail.com>
2024-03-14 12:16:28 +0100

    Final commit, home page new section added.

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,20 +7,26 @@ const Home = () => {
       <Header key={self.crypto.randomUUID()} />
       <div className="secondPart">
       <div className="sectionOne">
-          <div className="title">Recently added :</div>
+          <div className="title">New releases :</div>
         <img src="dune.jpg" alt="" />
         <h2>Dune: Part Two</h2>
         <p>Director : Denis Villeneuve</p>
         <p>Genre : science fiction film</p>
-        <p className="peacock">Watch it on : only in theaters</p>
         </div>
+        <div className="sectionOne">
+          <div className="title">New releases :</div>
+        <img src="demon-slayer-sanemi-obanai.jpg" alt="" />
+        <h2>Demon Slayer: To the Hashira Training</h2>
+        <p>Director : Haruo Sotozaki</p>
+        <p>Genres: Animation, Action, Fantasy</p>
+        </div>
+        
         <div className="sectionTwo">
           <div className="title">Award-winning title :</div>
         <img src="oppen.jpg" alt="" />
         <h2>Oppenheimer</h2>
         <p>Director and writer : Cristopher Nolan</p>
         <p>Genre : biographical drama film</p>
-        <p className="peacock">Watch it on : <a href="https://www.peacocktv.com/">Peacock</a></p>
         </div>
       </div>
     </>
